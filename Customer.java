1368c560390e19cce853eae90f6744c14836c75b
brenoam <brenoam@dcc.ufmg.br>
2019-05-26 21:02:05 -0300

    Extract Method

--- Customer.java
+++ Customer.java
@@ -13,6 +13,27 @@ class Customer {
    public String getName (){
       return _name;
    };
+   
+   private double amountFor(Rental rental){
+    double amount = 0;
+    switch (rental.getMovie().getPriceCode()) {
+           case Movie.REGULAR:
+              amount += 2;
+              if (rental.getDaysRented() > 2)
+                 amount += (rental.getDaysRented() - 2) * 1.5;
+              break;
+           case Movie.NEW_RELEASE:
+              amount += rental.getDaysRented() * 3;
+              break;
+           case Movie.CHILDRENS:
+              amount += 1.5;
+              if (rental.getDaysRented() > 3)
+                 amount += (rental.getDaysRented() - 3) * 1.5;
+               break;
+        }
+      return amount;
+   }
+   
   
   public String statement() {
      double totalAmount = 0;
@@ -24,21 +45,7 @@ class Customer {
         Rental each = (Rental) rentals.nextElement();
 
         //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
