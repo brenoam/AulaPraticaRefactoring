0283a0568c2ba330e34a8c8b09e161f51416de5a
brenoam <brenoam@dcc.ufmg.br>
2019-05-26 21:14:18 -0300

    Move Method

--- Customer.java
+++ Customer.java
@@ -1,68 +1,47 @@
 import java.util.*;
 class Customer {
-   private String _name;
-   private Vector<Rental> _rentals = new Vector<Rental>();
+	private String _name;
+	private Vector<Rental> _rentals = new Vector<Rental>();
 
-   public Customer (String name){
-      _name = name;
-   };
+	public Customer (String name){
+		_name = name;
+	};
 
-   public void addRental(Rental arg) {
-      _rentals.addElement(arg);
-   }
-   public String getName (){
-      return _name;
-   };
-   
-   private double amountFor(Rental aRental){
-    double amount = 0;
-    switch (aRental.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              amount += 2;
-              if (aRental.getDaysRented() > 2)
-                 amount += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              amount += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              amount += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 amount += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-      return amount;
-   }
-   
-  
-  public String statement() {
-     double totalAmount = 0;
-     int frequentRenterPoints = 0;
-     Enumeration rentals = _rentals.elements();
-     String result = "Rental Record for " + getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+	public String getName (){
+		return _name;
+	};
+	
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        thisAmount = amountFor(each);
+      //determine amounts for each line
+			thisAmount = each.getCharge();
 
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
+      // add frequent renter points
+			frequentRenterPoints ++;
+      // add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+				each.getDaysRented() > 1) frequentRenterPoints ++;
 
-        //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+      //show figures for this rental
+				result += "\t" + each.getMovie().getTitle()+ "\t" +
+			String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
 
-     }
-     //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
-             " frequent renter points";
-     return result;
-   }
+		}
+    //add footer lines
+		result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) +
+		" frequent renter points";
+		return result;
+	}
 }
--- Rental.java
+++ Rental.java
@@ -1,15 +1,34 @@
 class Rental {
-    private Movie _movie;
-    private int _daysRented;
+	private Movie _movie;
+	private int _daysRented;
 
-    public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-    }
-    public int getDaysRented() {
-      return _daysRented;
-    }
-    public Movie getMovie() {
-      return _movie;
-    }
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+	public int getDaysRented() {
+		return _daysRented;
+	}
+	public Movie getMovie() {
+		return _movie;
+	}
+	double getCharge(){
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				amount += 2;
+				if (getDaysRented() > 2)
+					amount += (getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				amount += getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				amount += 1.5;
+				if (getDaysRented() > 3)
+					amount += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return amount;
+	}
 }
